54778dfb08d9b53b6346ce69f19c5ec94f9aad49
tmrmotta <tmrmotta@outlook.com>
2024-10-01 18:43:18 -0300

    Testing CI with Docker

--- src-content/4-IntegracaoContinua/DockerCI/math_test.go
+++ src-content/4-IntegracaoContinua/DockerCI/math_test.go
@@ -1,8 +1,12 @@
 package main
 
+import "testing"
+
 func TestSoma(t *testing.T) {
-	total := Soma(15, 15)
+
+	total := soma(15, 15)
+
 	if total != 30 {
-		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: 30", total)
+		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
